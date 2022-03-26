ab45b076a7fea763d7cacd625af2d06215d1146f
LAZPbanahaker <pn0818x@outlok.com>
2022-03-26 20:21:44 +0800

    skills

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-
+import headVue from './components/head.vue'
+import skillsVue from './components/skills.vue';
 </script>
 
 <template>
-  
+  <headVue></headVue>
+  <skillsVue></skillsVue>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap');
+* {
+  font-family: 'Noto Sans TC', sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@master/normal.css';
-import '@master/styles';
 
 createApp(App).mount('#app')
