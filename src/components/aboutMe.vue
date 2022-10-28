0e255f40bd6e0441a948257659f980d3b681b6d0
banahaker <pn0818x@outlook.com>
2022-10-28 20:30:35 +0800

    fix: contetn

--- src/components/aboutMe.vue
+++ src/components/aboutMe.vue
@@ -5,12 +5,7 @@ import "animate.css";
 <template>
   <div class="aboutMe">
     <p class="animate__jello">
-      A 16-yo Senior High School Student from Taiwan, learned programming
-      language from 10 years old. My first program was written in C, the "hello,
-      world" let me say hello to the programming world! Then I keep learning
-      about all of programming field, and I enter the Web Develop when I was
-      12-yo. I learned much things from the programming community in Taiwan, and
-      I just founded a educational organization in the April this year(2022) -
+      A 16-yo Senior High School Student from Taiwan, who has learned programming language since 10 years old. My first program is written in C, the "hello, world" lets me say hello to the programming world! Then I keep learning about all of the programming field, and I entered the Web Developing when I was 12-yo. I have learned a lot of things from the programming community in Taiwan, and I just founded a educational organization in the April this year(2022) - 
       <a target="_blank" href="https://lipoic.org/">Lipoic</a>.
     </p>
   </div>
