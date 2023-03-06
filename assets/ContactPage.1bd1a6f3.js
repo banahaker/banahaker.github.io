32968a9244e52158bd61f292ca8d53075975be9b
banahaker <pn0818x@outlook.com>
2023-03-06 22:22:51 +0800

    Updates

---
+++ assets/ContactPage.1bd1a6f3.js
@@ -0,0 +1,1 @@
+import{P as t}from"./PageTitle.bde73745.js";import{d as e,i as o,o as a}from"./index.19923890.js";const l=e({__name:"ContactPage",setup(n){return(c,r)=>(a(),o(t,{title:"Contact",subtitle:"Contact me by following way"}))}});export{l as default};
