20f3bf3be74ebf358ca9271ad89545a014a78fd3
banahaker <pn0818x@outlook.com>
2023-03-06 23:20:57 +0800

    feat: skillbar components

---
+++ src/components/Resume/SkillBar.vue
@@ -0,0 +1,91 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  skill: string;
+  score: number;
+}>();
+</script>
+
+<template>
+  <div class="skillbar">
+    <div class="skillbar-title">
+      <span>{{ props.skill }}</span>
+    </div>
+    <div class="skillbar-bar" :style="`width: ${(score / 5) * 100}%`"></div>
+    <div className="skill-bar-percent">{{ props.score }} / 5</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.skillbar {
+  position: relative;
+  display: block;
+  margin-bottom: 15px;
+  width: 100%;
+  background: #eee;
+  height: 35px;
+  border-radius: 3px;
+  -moz-border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -webkit-transition: 0.4s linear;
+  -moz-transition: 0.4s linear;
+  -ms-transition: 0.4s linear;
+  -o-transition: 0.4s linear;
+  transition: 0.4s linear;
+  -webkit-transition-property: width, background-color;
+  -moz-transition-property: width, background-color;
+  -ms-transition-property: width, background-color;
+  -o-transition-property: width, background-color;
+  transition-property: width, background-color;
+}
+
+.skillbar-title {
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-weight: bold;
+  font-size: 13px;
+  color: #fff;
+  background: #04a799;
+  -webkit-border-top-left-radius: 3px;
+  -webkit-border-bottom-left-radius: 4px;
+  -moz-border-radius-topleft: 3px;
+  -moz-border-radius-bottomleft: 3px;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+
+.skillbar-title span {
+  display: block;
+  background: rgba(0, 0, 0, 0.1);
+  color: #fff;
+  color: rgb(255, 255, 255);
+  padding: 0 20px;
+  height: 35px;
+  line-height: 35px;
+  -webkit-border-top-left-radius: 3px;
+  -webkit-border-bottom-left-radius: 3px;
+  -moz-border-radius-topleft: 3px;
+  -moz-border-radius-bottomleft: 3px;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+
+.skillbar-bar {
+  height: 35px;
+  background: #04a799;
+  border-radius: 3px;
+  -moz-border-radius: 3px;
+  -webkit-border-radius: 3px;
+}
+
+.skill-bar-percent {
+  position: absolute;
+  right: 10px;
+  top: 0;
+  font-size: 11px;
+  height: 35px;
+  line-height: 35px;
+  color: #444;
+  color: rgba(0, 0, 0, 0.4);
+}
+</style>
--- src/pages/ResumePage.vue
+++ src/pages/ResumePage.vue
@@ -1,7 +1,63 @@
 <script lang="ts" setup>
 import PageTitle from "../components/Layout/PageTitle.vue";
+import SkillBar from "../components/Resume/SkillBar.vue";
+import ExData from "../article/exlist.json";
 </script>
 
 <template>
   <PageTitle title="resume" subtitle=""></PageTitle>
+  <div class="page resume">
+    <div class="page-section">
+      <h3>education</h3>
+      <ul>
+        <li>Mingdao High School Senior High Department</li>
+      </ul>
+    </div>
+    <div class="page-section">
+      <h3>experience</h3>
+      <div class="ex-list">
+        <div class="ex-item" v-for="item in ExData">
+          <h4>{{ item.title }}</h4>
+          <p class="time">{{ item.time }}</p>
+          <ul>
+            <li v-for="point in item.content">
+              {{ point }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="page-section">
+      <h3>SKILLS</h3>
+      <SkillBar skill="Python" :score="5"></SkillBar>
+      <SkillBar skill="Numpy" :score="4"></SkillBar>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+h3 {
+  display: block;
+  width: 100%;
+  text-align: center;
+  margin: 20px 0;
+}
+
+.resume {
+  .ex-list {
+    .ex-item {
+      h4 {
+        font-size: 1rem;
+        font-weight: 500;
+        letter-spacing: 3px;
+        margin: 5px 0;
+      }
+      p.time {
+        font-size: 0.8rem;
+        font-weight: 200;
+        letter-spacing: 2px;
+      }
+    }
+  }
+}
+</style>
