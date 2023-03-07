9371440939930233e3abff59dabc1da914607d42
banahaker <pn0818x@outlook.com>
2023-03-07 20:17:50 +0800

    feat: contact page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup>
+import LinkBlank from "../components/Global/LinkBlank.vue";
 import PageTitle from "../components/Layout/PageTitle.vue";
 </script>
 
 <template>
   <PageTitle title="Contact" subtitle="Contact me by following way"></PageTitle>
+  <div class="page">
+    <div class="page-section">
+      <p>Feel free to get in touch. You can email me at:</p>
+      <LinkBlank link="mailto:lazpytb@gmail.com">lazpytb@gmail.com</LinkBlank>
+    </div>
+  </div>
 </template>
