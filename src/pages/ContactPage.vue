150d235f6f2c332a3158269aa2c540240471475a
banahaker <pn0818x@outlook.com>
2023-03-08 10:36:16 +0800

    feat: contact message

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -9,6 +9,11 @@ import PageTitle from "../components/Layout/PageTitle.vue";
     <div class="page-section">
       <p>Feel free to get in touch. You can email me at:</p>
       <LinkBlank link="mailto:lazpytb@gmail.com">lazpytb@gmail.com</LinkBlank>
+      <p>
+        For speech, teaching and other cooperations, please contact me with
+        above email.
+      </p>
+      <p>任何演講、教學或是合作邀約，請以上述 Email 聯繫我</p>
     </div>
   </div>
 </template>
