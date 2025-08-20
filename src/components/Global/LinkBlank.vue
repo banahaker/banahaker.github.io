d4b8dba2ffc1e57ba5cdb60dfbf2c86a4c9ee080
banahaker <pn0818x@outlook.com>
2025-08-20 22:05:56 +0800

    REMOVE ALL

--- src/components/Global/LinkBlank.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup>
-const { link } = defineProps<{ link: string }>();
-</script>
-
-<template>
-  <a :href="link" target="_blank"><slot></slot></a>
-</template>
--- src/interface/CategortType.ts
+++
@@ -1,5 +0,0 @@
-export enum Category {
-  WEB_TOOLS = 0,
-  DATA_SCIENCE,
-  OTHER,
-}
