c987354a4620b99cf12a2a1dc2dabcbd29193a55
banahaker <pn0818x@outlook.com>
2025-08-20 23:14:39 +0800

    Refactor application to use Tailwind CSS and update profile component
    - Removed existing CSS styles from App.css and replaced with Tailwind CSS import.
    - Simplified App.tsx to include a new ProfileBlock component.
    - Added a new logo image (logo_bana.png) for the profile.
    - Deleted unused react.svg asset.
    - Created ProfileBlock component to display user information and avatar.
    - Cleaned up index.css by removing unnecessary styles and added Google Fonts.
    - Updated vite.config.ts to include Tailwind CSS plugin.

---
+++ src/components/profile.tsx
@@ -0,0 +1,17 @@
+import logo_bana from "../assets/logo_bana.png";
+
+export default function ProfileBlock() {
+  return (
+    <>
+      <div>
+        <img src={logo_bana} alt="Lazp's Avatar" />
+        <div>
+          <h1>Lazp Yeh</h1>
+          <a href="mailto:lazyeh.official@gmail.com">
+            lazyeh.official@gmail.com
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
