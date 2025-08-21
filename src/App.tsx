6806fcad2f48ff4c446d813916d190f0760b1e47
banahaker <pn0818x@outlook.com>
2025-08-21 21:49:08 +0800

    style: update layout and styling for ProfileBlock component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-[80dvw] mx-auto">
+    <div className="w-[90dvw] max-w-[800px] mx-auto">
       <ProfileBlock></ProfileBlock>
     </div>
   );
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -3,12 +3,19 @@ import logo_bana from "../assets/logo_bana.png";
 export default function ProfileBlock() {
   return (
     <>
-      <div>
-        <img src={logo_bana} alt="Lazp's Avatar" />
-        <div>
-          <h1>Lazp Yeh</h1>
-          <a href="mailto:lazyeh.official@gmail.com">
-            lazyeh.official@gmail.com
+      <div className="w-full flex gap-8 items-center justify-center p-8 bg-gray-100 rounded-2xl mt-6">
+        <img
+          src={logo_bana}
+          alt="Lazp's Avatar"
+          className="rounded-full w-[128px] h-[128px]"
+        />
+        <div className="flex flex-col gap-4">
+          <h1 className="text-4xl font-bold">Lazp Yeh</h1>
+          <a
+            className="ibm-plex-mono transition-all duration-200 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-blue-600 hover:to-pink-600"
+            href="mailto:lazpytb@gmail.com"
+          >
+            lazpytb@gmail.com
           </a>
         </div>
       </div>
