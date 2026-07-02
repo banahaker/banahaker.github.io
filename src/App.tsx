5dc6a1bf33a021bec20ee05671109a781ca3ca09
banahaker <pn0818x@outlook.com>
2026-07-02 22:07:55 +0800

    feat: two-column sidebar + timeline app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import ProfileBlock from "./components/ProfileBlock";
+import Sidebar from "./components/Sidebar";
 import IntroBlock from "./components/IntroBlock";
-import EducationExpBlock from "./components/EducationExpBlock";
 import WorkExpBlock from "./components/WorkExpBlock";
-import CompetitionExpBlock from "./components/CompetitionExpBlock";
+import EducationExpBlock from "./components/EducationExpBlock";
 import ProjectExpBlock from "./components/ProjectExpBlock";
+import CompetitionExpBlock from "./components/CompetitionExpBlock";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="w-[90dvw] max-w-[800px] mx-auto">
-      <ProfileBlock></ProfileBlock>
-      <IntroBlock></IntroBlock>
-      <EducationExpBlock></EducationExpBlock>
-      <WorkExpBlock></WorkExpBlock>
-      <CompetitionExpBlock></CompetitionExpBlock>
-      <ProjectExpBlock></ProjectExpBlock>
+    <div className="min-h-dvh">
+      <div className="mx-auto w-[90dvw] max-w-[1080px] py-12 md:py-20 flex flex-col md:flex-row gap-12 md:gap-16">
+        <div className="md:w-[300px] md:shrink-0">
+          <Sidebar />
+        </div>
+        <main className="min-w-0 flex-1">
+          <IntroBlock />
+          <div className="mt-12 flex flex-col">
+            <WorkExpBlock />
+            <EducationExpBlock />
+            <ProjectExpBlock />
+            <CompetitionExpBlock />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
